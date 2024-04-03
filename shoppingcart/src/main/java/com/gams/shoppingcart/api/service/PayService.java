771565871ed5d05253a0dffc51d25711ddcc1ad9
genaro1024 <genaro1024@hotmail.com>
2024-04-03 14:14:09 -0600

    calculation corrections

--- shoppingcart/src/main/java/com/gams/shoppingcart/api/service/PayService.java
+++ shoppingcart/src/main/java/com/gams/shoppingcart/api/service/PayService.java
@@ -6,13 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.gams.shoppingcart.api.entity.Detail;
 import com.gams.shoppingcart.api.entity.Pay;
+import com.gams.shoppingcart.api.entity.Product;
+import com.gams.shoppingcart.api.entity.PurchaseOrder;
+import com.gams.shoppingcart.api.repository.DetailRepository;
+import com.gams.shoppingcart.api.repository.OrderRepository;
 import com.gams.shoppingcart.api.repository.PayRepository;
 
 @Service
 public class PayService {
     @Autowired
-    private PayRepository payRepository; 
+    private PayRepository payRepository;
+    @Autowired
+    private OrderRepository orderRepository; 
+    @Autowired
+    private DetailRepository detailRepository;      
+    @Autowired
+    private ProductServiceClient productServiceClient;    
 
     public Iterable<Pay> getAllPays(){ 
         return payRepository.findAll();
@@ -22,8 +33,19 @@ public class PayService {
         return payRepository.findById(id);
     }
 
-    public Pay savePay(Pay purchaseOrder){ 
-        return payRepository.save(purchaseOrder);
+    public Pay savePay(Pay pay){ 
+        long orderId = pay.getOrder().getId();
+        PurchaseOrder order = orderRepository.findById(orderId).get();
+        Iterable<Detail> details = detailRepository.findByOrderId(order.getId());
+        float amount = 0.0f;
+        for (Detail detail : details) {
+            long product_id = detail.getProductId();
+            Product product = productServiceClient.getProductById(product_id).block();
+            float price = product.getPrice();
+            amount+=price*detail.getQuantity();
+        }
+        pay.setAmount(amount);
+        return payRepository.save(pay);
     }
 
     public Pay updatePay(Long id, Pay updatePay) throws NotFoundException {
